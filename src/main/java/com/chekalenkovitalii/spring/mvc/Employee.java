f19a62c28ecd899fc97187b813c1d7456773b002
Chekalenko Vitaliy <chekalenkovitaliy@gmail.com>
2021-01-09 15:39:36 +0200

    Spring MVC: @RequestMapping, @ModelAttribute, input form l.62

---
+++ src/main/java/com/chekalenkovitalii/spring/mvc/Employee.java
@@ -0,0 +1,54 @@
+package com.chekalenkovitalii.spring.mvc;
+
+public class Employee {
+
+    private String name ;
+    private String surname;
+    private int salary;
+    private String department;
+
+    public Employee() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", salary=" + salary +
+                ", department='" + department + '\'' +
+                '}';
+    }
+}
--- src/main/java/com/chekalenkovitalii/spring/mvc/MyController.java
+++ src/main/java/com/chekalenkovitalii/spring/mvc/MyController.java
@@ -3,12 +3,13 @@ package com.chekalenkovitalii.spring.mvc;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import javax.servlet.http.HttpServletRequest;
 
 @Controller
+@RequestMapping("/employee")
 public class MyController {
 
     @RequestMapping("/")
@@ -16,30 +17,15 @@ public class MyController {
         return "first-view";
     }
 
-    @RequestMapping("askDetails")
-    public String askEmployeeDetails(){
+    @RequestMapping("/askDetails")
+    public String askEmployeeDetails(Model model){
+        model.addAttribute("employee", new Employee());
+
         return "ask-emp-details-view";
     }
 
-//    @RequestMapping("showDetails")
-//    public String showEmpDetails(HttpServletRequest request, Model model){
-//
-//        String empName = request.getParameter("employeeName");
-//        empName = "MR. " + empName;
-//        model.addAttribute("nameAttribute", empName);
-//
-//        model.addAttribute("description", "SACO-udemy");
-//
-//        return "show-emp-details-view";
-//    }
-
-    @RequestMapping("showDetails")
-    public String showEmpDetails(@RequestParam("employeeName") String empName, Model model){
-
-        empName = "MR. " + empName+ " !!!";
-        model.addAttribute("nameAttribute", empName);
-
-
+    @RequestMapping("/showDetails")
+    public String showEmpDetails(@ModelAttribute("employee") Employee emp){
 
         return "show-emp-details-view";
     }
